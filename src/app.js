91f1527b5fc39e78e1bd3387000ad2dda1cdbbf0
Erik Nogueira <eriknoguei30@gmail.com>
2026-09-03 21:39:36 -0300

    Update health check message to include 'XD'

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Rota de teste / health check
 app.get('/', (req, res) => {
-  res.json({ mensagem: 'Erik Nogueira' });
+  res.json({ mensagem: 'Erik Nogueira XD' });
 });
 
 app.use('/usuarios', usuariosRoutes);
